c17d4b50fc761a9d51345b57b96616c38cab6e64
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-05-24 09:55:30 -0300

    Adicionando validações nos campos do endereço

--- test_cadastro.py
+++ test_cadastro.py
@@ -7,7 +7,11 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['DATABASE_USER'] = 'test'
+    app.config['DATABASE_HOST'] = 'localhost'
+    app.config['DATABASE_NAME'] = 'hub_solidario_desenvolvimento_tests'
+    app.config['DATABASE_USER'] = 'postgres'
+    app.config['DATABASE_PASSWORD'] = 'default_123'
+    app.config['APP_SETTINGS'] = 'config.TestsConfig'
     app.config['TESTING'] = True
 
     with app.test_client() as client:
@@ -117,3 +121,348 @@ def test_descricao_deve_possuir_no_maximo_1000_caracteres(client):
 
     assert response.status_code == 400
     assert 'Descrição deve possuir no máximo 1000 caracteres' in data['errors']
+
+def test_cep_deve_ser_preenchido(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "",
+        "rua": "",
+        "numero": "",
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'CEP é obrigatório' in data['errors']
+
+def test_cep_deve_possuir_no_maximo_8_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "",
+        "numero": "",
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'CEP deve possuir no máximo 8 caracteres' in data['errors']
+
+def test_rua_deve_ser_preenchida(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "",
+        "numero": "",
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Rua/Avenida é obrigatória' in data['errors']
+
+def test_rua_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "R" * 101,
+        "numero": "",
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Rua/Avenida deve possuir no máximo 100 caracteres' in data['errors']
+
+def test_numero_deve_ser_preenchido(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "",
+        "numero": "",
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Número é obrigatório' in data['errors']
+
+def test_numero_deve_possuir_no_maximo_50_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "1" * 51,
+        "complemento": "",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Número deve possuir no máximo 50 caracteres' in data['errors']
+
+def test_complemento_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "C" * 101,
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Complemento deve possuir no máximo 100 caracteres' in data['errors']
+
+def test_bairro_deve_ser_preenchido(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Bairro é obrigatório' in data['errors']
+
+def test_bairro_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "B" * 101,
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Bairro deve possuir no máximo 100 caracteres' in data['errors']
+
+def test_cidade_deve_ser_preenchida(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Cidade é obrigatória' in data['errors']
+
+def test_cidade_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "C" * 101,
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Cidade deve possuir no máximo 100 caracteres' in data['errors']
+
+def test_estado_deve_ser_preenchido(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "Cidade Teste",
+        "estado": "",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Estado é obrigatório' in data['errors']
+
+def test_estado_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "Cidade Teste",
+        "estado": "E" * 101,
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'Estado deve possuir no máximo 100 caracteres' in data['errors']
+
+def test_pais_deve_ser_preenchido(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "Cidade Teste",
+        "estado": "Estado Teste",
+        "pais": "",
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'País é obrigatório' in data['errors']
+
+def test_pais_deve_possuir_no_maximo_100_caracteres(client):
+    data = {
+        "nome": "Instituição Teste",
+        "cnpj": "01803408000164",
+        "descricao": "Descrição Teste",
+        "categoria": 6,
+        "exibirEndereco": "S",
+        "cep": "010010001",
+        "rua": "Rua Teste",
+        "numero": "123",
+        "complemento": "Complemento Teste",
+        "bairro": "Bairro Teste",
+        "cidade": "Cidade Teste",
+        "estado": "Estado Teste",
+        "pais": "P" * 101,
+    }
+    response = client.post("/cadastrar", data=data)
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert 'País deve possuir no máximo 100 caracteres' in data['errors']
